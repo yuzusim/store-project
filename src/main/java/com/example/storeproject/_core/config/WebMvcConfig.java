dd8e679bec9986e3ed22898e6fe355c7467025c5
yuzusim <1625787@donga.ac.kr>
2024-04-23 20:25:16 +0900

    사진등록

---
+++ src/main/java/com/example/storeproject/_core/config/WebMvcConfig.java
@@ -0,0 +1,32 @@
+package com.example.storeproject._core.config;
+
+import com.example.storeproject.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+@Configuration // IoC
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+
+        registry
+                .addResourceHandler("/upload/**")
+                .addResourceLocations("file:./upload/")
+                .setCachePeriod(60 * 60) // 초 단위 => 한시간
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/board/**", "/user/**")
+                .excludePathPatterns("/board/{id:\\d+}");
+
+    }
+}
--- src/main/java/com/example/storeproject/product/ProductRequest.java
+++ src/main/java/com/example/storeproject/product/ProductRequest.java
@@ -1,8 +1,12 @@
 package com.example.storeproject.product;
 
+import com.example.storeproject._core.common.ImgSaveUtil;
+import com.example.storeproject.user.User;
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
 
 public class ProductRequest {
+
     @Data
     public static class UpdateDTO {
         private String name;
@@ -15,15 +19,17 @@ public class ProductRequest {
         private String name;
         private Integer price;
         private Integer qty;
+        private MultipartFile img;
 
-        public Product toEntity() {
+        public Product toEntity(User user) {
+            String imgFileName = ImgSaveUtil.save(img);
             return Product.builder()
+                    .img(imgFileName)
                     .name(name)
                     .price(price)
                     .qty(qty)
-                    //.user(user)
+                    .user(user)
                     .build();
-
         }
 
     }
